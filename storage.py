c6ebebfa9552104e8f9eda5b35c7b9b98107947a
Viktor7772001 <Skoklo123@mail.ru>
2025-02-25 00:28:41 +0500

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,20 +1,18 @@
 class Message:
     def __init__(self, sender_id, sender_name, text, chat_id):
-        self.sender_id = sender_id  # ID отправителя
-        self.sender_name = sender_name  # Имя отправителя
-        self.text = text  # Текст сообщения
-        self.chat_id = chat_id  # ID чата
+        self.sender_id = sender_id
+        self.sender_name = sender_name
+        self.text = text
+        self.chat_id = chat_id
 
 class MessageStorage:
     def __init__(self):
-        self.messages = []  # Список сообщений
-        self._custom_names = {}  # Пользовательские имена
-        self._rename_states = {}  # Состояния переименования
+        self.messages = []
+        self._custom_names = {}
+        self._rename_states = {}
 
     def add_message(self, message):
-        self.messages.append(message)  # Добавить сообщение
+        self.messages.append(message)
 
     def clear(self):
-        self.messages.clear()  # Очистить всё
-
-    # ... остальные методы (пока не важно)
+        self.messages.clear()
